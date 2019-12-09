251a4e8f86e021bb0e465e4b1387c965290cc594
DEEPU <DEEPU@DEEPUHOME>
2019-12-09 21:36:03 +0530

    azure push test

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-  DB_URI: process.env.DB_URI,
+  //DB_URI: process.env.DB_URI,
+  DB_URI: 'mongodb://pradeep:deepu1982@ds018839.mlab.com:18839/mpkportfolio',
+  SECRET: 'deepu',
   NAMESPACE: 'https://filipjerga.herokuapp.com'
-}
-
+};
